f7e1789a4583716c89db3825041bd0d5bdf0be14
lyt-create <68070072+lyt-create@users.noreply.github.com>
2022-03-05 13:08:49 +0800

    Update settings.py

--- ele/settings.py
+++ ele/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7bc!3+l+pw9+dq2xck4a!an3q!del$fv-lw^1+42)h(8xxm%ay'
+SECRET_KEY = 'django-insecure-7bc!3+l+pw9+dq2xck4a!an3q!del$fv-lw^1+42)h(8yyy%ay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
